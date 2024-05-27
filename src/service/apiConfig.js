2e8ebe0b2087ee1a2dba61a911aa540436c7b816
lakmal05 <lakmal@webmotech.com>
2024-05-27 10:25:58 +0530

    merged from development

--- src/service/apiConfig.js
+++ src/service/apiConfig.js
@@ -3,6 +3,7 @@ const URL_REMOTE = `http://84.46.249.96:4008`; //server
 // const URL_REMOTE = `http://192.168.1.42:4008`;
 // const URL_REMOTE = `http://localhost:4008`;
 
+
 const adminPrefix = "ADMIN";
 const conf = {
   serverUrl: URL_REMOTE,
