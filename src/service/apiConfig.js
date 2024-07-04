13b366bb3019da483848a99efd443646daa14700
lakmal05 <lakmal@webmotech.com>
2024-07-04 13:09:00 +0530

    url

--- src/service/apiConfig.js
+++ src/service/apiConfig.js
@@ -1,7 +1,6 @@
 const URL_REMOTE = "https://adress-api.webmotech.com"; //server
 // const URL_REMOTE = `http://127.0.0.1:4008`;  // lakmal ip
-// const URL_REMOTE = `http://192.168.1.42:4008`;
-// const URL_REMOTE = `http://localhost:4008`;
+// const URL_REMOTE = "http://45.55.37.84:4002"; //server
 
 const adminPrefix = "ADMIN";
 const conf = {
