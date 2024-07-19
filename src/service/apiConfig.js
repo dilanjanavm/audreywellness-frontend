f54b4e16fe169e5b5b833a0380bddaa043524c08
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-07-19 16:17:26 +0530

    create seperate envs and manage them when run the project and create build file

--- src/service/apiConfig.js
+++ src/service/apiConfig.js
@@ -1,4 +1,6 @@
-const URL_REMOTE = "https://adress-api.webmotech.com"; //server
+const URL_REMOTE = process.env.REACT_APP_API_URL;
+
+// const URL_REMOTE = "https://adress-api.webmotech.com"; //server
 // const URL_REMOTE = `http://127.0.0.1:4008`;  // lakmal ip
 // const URL_REMOTE = "http://45.55.37.84:4002"; //server
 
@@ -6,7 +8,7 @@ const adminPrefix = "ADMIN";
 const conf = {
   serverUrl: URL_REMOTE,
   basePath: `public`,
-  redirect: `http://127.0.0.1:4008`,
+  redirect: URL_REMOTE,
   adminPrefix: adminPrefix,
 };
 
