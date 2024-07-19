73a9bb26fab17dc89b0a4a185ca3a7c609aaca6a
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-07-19 17:34:26 +0530

    add service to category diltartion

--- src/service/categoryService.js
+++ src/service/categoryService.js
@@ -23,12 +23,12 @@ export async function getAllCategoriesWithOrWithoutSubCategories(
   return await ApiService.callApi(apiObject);
 }
 
-export async function getAllCategories() {
+export async function getAllCategories(currentPage) {
   const apiObject = {};
   apiObject.method = "GET";
   apiObject.authentication = true;
   apiObject.isWithoutPrefix = false;
-  apiObject.endpoint = "api/category/find-all";
+  apiObject.endpoint = `api/category/find-all?perPage=${15}&page=${currentPage}`;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
@@ -57,10 +57,22 @@ export async function updateCategory(id, data) {
 
 export async function deleteCategory(catId) {
   const apiObject = {};
-  apiObject.method = "DELETE",
-  apiObject.authentication = true,
-  apiObject.isWithoutPrefix = false;
+  (apiObject.method = "DELETE"),
+    (apiObject.authentication = true),
+    (apiObject.isWithoutPrefix = false);
   apiObject.endpoint = ``;
+  apiObject.body = null;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function categoryFiltration(data, currentPage) {
+  const apiObject = {};
+  apiObject.method = "GET";
+  apiObject.authentication = true;
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = `api/category/find-all?name=${data?.name}&status=${
+    data?.status
+  }&perPage=${15}&page=${currentPage}`;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
