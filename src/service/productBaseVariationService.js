33738f56dce15ee961d5f0202a5162ff2bdc47cf
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-07-22 12:30:59 +0530

    update and add endpoint in product service and productbaseVariation service

--- src/service/productBaseVariationService.js
+++ src/service/productBaseVariationService.js
@@ -1,21 +1,31 @@
 import ApiService from "./apiService";
 
-export async function getAllProductBaseVariation() {
+export async function getAllProductBaseVariation(currentPage) {
   const apiObject = {};
-  (apiObject.method = "GET"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
-  apiObject.endpoint = `api/admin/product-base-variant/find-all`;
+  apiObject.method = "GET",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = `api/admin/product-base-variant/find-all?perPage=${15}&page=${currentPage}`;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
 
 export async function getProductBaseVariationDetailsById(productId) {
   const apiObject = {};
-  (apiObject.method = "GET"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "GET",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = `api/admin/product-base-variant/find-by-id/${productId}`;
+  apiObject.body = null;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function productBaseVariationFiltration(data,currentPage) {
+  const apiObject = {};
+  apiObject.method = "GET",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = `api/admin/product-base-variant/find-all?name=${data.name}&category=${data.category}&status=${data.status}perPage=${15}&page=${currentPage}`;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
--- src/service/productService.js
+++ src/service/productService.js
@@ -2,19 +2,19 @@ import ApiService from "./apiService";
 
 // export async function getAllProducts() {
 //   const apiObject = {};
-//   (apiObject.method = "GET"),
-//     (apiObject.authentication = true),
-//     (apiObject.isWithoutPrefix = false);
-//   apiObject.endpoint = `api/product/find-all`;
+//   apiObject.method = "GET",
+//   apiObject.authentication = true,
+//   apiObject.isWithoutPrefix = false;
+//   apiObject.endpoint = `api/product/find-all?perPage=${15}&page=${currentPage}`;
 //   apiObject.body = null;
 //   return await ApiService.callApi(apiObject);
 // }
 
 export async function addNewProduct(data) {
   const apiObject = {};
-  (apiObject.method = "POST"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "POST",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = `api/admin/product/create`;
   apiObject.body = data;
   return await ApiService.callApi(apiObject);
@@ -22,9 +22,9 @@ export async function addNewProduct(data) {
 
 export async function updateProduct(productId, data) {
   const apiObject = {};
-  (apiObject.method = "PUT"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "PUT",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = ``;
   apiObject.body = data;
   return await ApiService.callApi(apiObject);
@@ -32,21 +32,19 @@ export async function updateProduct(productId, data) {
 
 export async function deleteProduct(productId) {
   const apiObject = {};
-  (apiObject.method = "DELETE"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "DELETE",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = ``;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
 
-
-
 export async function checkProductNameExists(productName) {
   const apiObject = {};
-  (apiObject.method = "GET"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "GET",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = `api/admin/product/find-by-name/${productName}`;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
@@ -54,24 +52,20 @@ export async function checkProductNameExists(productName) {
 
 export async function activeInactiveProduct(productId, status) {
   const apiObject = {};
-  (apiObject.method = "PATCH"),
-    (apiObject.authentication = true),
-    (apiObject.isWithoutPrefix = false);
+  apiObject.method = "PATCH",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
   apiObject.endpoint = ``;
   apiObject.body = null;
   return await ApiService.callApi(apiObject);
 }
 
-// export async function searchProductsFiltration(data, currentPage) {
+// export async function productsFiltration(data, currentPage) {
 //   const apiObject = {};
 //   apiObject.method = "GET";
 //   apiObject.authentication = true;
 //   apiObject.isWithoutPrefix = false;
-//   apiObject.endpoint = `api/product/get-all-products?name=${
-//     data.name
-//   }&category=${data.category}&status=${
-//     data.status
-//   }&limit=${24}&pages=${currentPage}`;
+//   apiObject.endpoint = `api/product/find-all?name=${data.name}&category=${data.category}&status=${data.status}&perPage=${15}&page=${currentPage}`;
 //   apiObject.body = null;
 //   return await ApiService.callApi(apiObject);
 // }
